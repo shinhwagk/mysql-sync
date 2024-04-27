751f09ee076036c7cc8c2bc7e4d32962d3663d54
张徐 <shinhwagk@outlook.com>
2024-04-27 17:58:09 +0800

    Update test_data.sql

--- test_data.sql
+++ test_data.sql
@@ -16,7 +16,7 @@ insert into test.tab1 values(1,'a');
 insert into test.tab1 values(2,'a');
 insert into test.tab1 values(3,'a');
 flush logs;
-start transation;
+start transcation;
 delete from test.tab1 where a=1;
 update test.tab1 set b='b' where a=2;
 replace into test.tab1 values(3,'c');
