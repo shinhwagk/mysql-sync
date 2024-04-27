7c6990fcb29e77ca2af47b3121d74379662de633
张徐 <shinhwagk@outlook.com>
2024-04-27 21:23:38 +0800

    check

--- initContainer.bash.sh
+++ initContainer.bash.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-dnf install -y jq diffutils git
+dnf install -y jq diffutils git shellcheck
 
 dnf install -y https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm
 dnf install -y mysql-community-client
