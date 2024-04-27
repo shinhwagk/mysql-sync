c9df53b65a9e785b571acaffbbc5a9d04b71ec14
gk <shinhwagk@outlook.com>
2024-04-27 16:43:03 +0800

    Update initContainer.bash.sh

--- initContainer.bash.sh
+++ initContainer.bash.sh
@@ -1,1 +1,8 @@
-dnf install -y jq diffutils
+#!/usr/bin/env bash
+
+set -e
+
+dnf install -y jq diffutils git
+
+dnf install -y https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm
+dnf install -y mysql-community-client
