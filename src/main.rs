32a2a8820c71f4c1aca9ba1a9325c324309013da
gk <shinhwagk@outlook.com>
2024-04-28 21:07:46 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,39 +35,6 @@ struct Event_query {
 }
 use std::time::{Duration, Instant};
 
-fn print_statistics() {
-    let stats_write_rows = 10;
-    let stats_delete_rows = 5;
-    let stats_update_rows = 7;
-    let stats_table_map = 2;
-    let stats_gtid_next = "placeholder-gtid";
-    let stats_commit = 3;
-    let stats_rollback = 1;
-    let stats_timestamp = "2023-10-07T12:34:56Z";
-    let stats_at = 100;
-    let stats_binlog = 1;
-    let stats_binlogfile = "log001.bin";
-    let stats_lines = 50;
-
-    let json_string = format!(
-        r#"{{"Write_rows": {}, "Delete_rows": {}, "Update_rows": {}, "Table_map": {}, "GTID_NEXT": "{}", "COMMIT": {}, "ROLLBACK": {}, "TIMESTAMP": "{}", "at": {}, "BINLOG": {}, "BINLOGFILE": "{}", "lines": {}}}"#,
-        stats_write_rows,
-        stats_delete_rows,
-        stats_update_rows,
-        stats_table_map,
-        stats_gtid_next,
-        stats_commit,
-        stats_rollback,
-        stats_timestamp,
-        stats_at,
-        stats_binlog,
-        stats_binlogfile,
-        stats_lines
-    );
-
-    println!("{}", json_string);
-}
-
 fn process_line(stdin_lock: std::io::StdinLock) {
     let mut update_rows: HashMap<String, i32> = HashMap::new();
     let mut delete_rows: HashMap<String, i32> = HashMap::new();
@@ -79,6 +46,9 @@ fn process_line(stdin_lock: std::io::StdinLock) {
     let mut table_id: String = String::new();
 
     let mut binlog_pos: String = String::new();
+    let mut binlog_file: String = String::new();
+
+    let mut commit: u32 = 0;
 
     let mut last_time = Instant::now();
 
@@ -92,88 +62,101 @@ fn process_line(stdin_lock: std::io::StdinLock) {
                 // if last_time.elapsed() >= Duration::from_secs(1) {
                 let total: i32 = write_rows.values().sum();
 
-                let json_string = format!(r#"{{"Write_rows": {}}}"#, total);
+                let json_string = format!(
+                    r#"{{"Write_rows": {}, "Binlog_pos": {}, "Binlog_file": {}, "Commit":{}}}"#,
+                    total, binlog_pos, binlog_file, commit
+                );
 
                 eprintln!("{}", json_string);
                 //     last_time = Instant::now();
                 // }
+                loop {
+                    match binlog_state {
+                        BinlogState::Normal => {
+                            if line.starts_with("# at ") {
+                                let pos = &line[5..];
+                                binlog_pos = pos.to_string();
+                                // eprintln!("{}", binlog_pos)
+                            } else if line.starts_with("#2") {
+                                let words: Vec<&str> = line.split_whitespace().collect();
 
-                match binlog_state {
-                    BinlogState::Normal => {
-                        if line.starts_with("# at ") {
-                            let pos = &line[5..];
-                            binlog_pos = pos.to_string();
-                            // eprintln!("{}", binlog_pos)
-                        } else if line.starts_with("#2") {
-                            let words: Vec<&str> = line.split_whitespace().collect();
-
-                            if words.len() >= 10 && words[7] == "CRC32" {
-                                match words[9] {
-                                    "Start:" | "Previous-GTIDs" | "Stop" | "GTID" => {}
-                                    "Xid" => {}
-                                    "Query" => {
-                                        if words.len() == 13 {
-                                            // eprintln!("111 {:?}", words)
-                                        } else if words.len() == 16 {
-                                            // eprintln!("xid {}", words[13])
+                                if words.len() >= 10 && words[7] == "CRC32" {
+                                    match words[9] {
+                                        "Start:" | "Previous-GTIDs" | "Stop" | "GTID" => {}
+                                        "Xid" => {}
+                                        "Query" => {
+                                            if words.len() == 13 {
+                                                // eprintln!("111 {:?}", words)
+                                                commit += 1
+                                            } else if words.len() == 16 {
+                                                // eprintln!("xid {}", words[13])
+                                            }
                                         }
-                                    }
-                                    "Write_rows:" => {
-                                        if words[12].to_string() == table_id {
-                                            *write_rows.entry(table_map.clone()).or_insert(0) += 1;
+                                        "Write_rows:" => {
+                                            if words[12].to_string() == table_id {
+                                                *write_rows
+                                                    .entry(table_map.clone())
+                                                    .or_insert(0) += 1;
+                                            }
                                         }
+                                        "Delete_rows:" => {
+                                            *delete_rows.entry(table_map.clone()).or_insert(0) += 1;
+                                        }
+                                        "Update_rows:" => {
+                                            *update_rows.entry(table_map.clone()).or_insert(0) += 1;
+                                        }
+                                        "Table_map:" => {
+                                            table_map = words[10].replace("`", "");
+                                            table_id = words[14].to_string();
+                                        }
+                                        "Rotate" if words.get(10) == Some(&"to") => {
+                                            // eprintln!("222 {}", words[11])
+                                        }
+                                        _ => {}
                                     }
-                                    "Delete_rows:" => {
-                                        *delete_rows.entry(table_map.clone()).or_insert(0) += 1;
-                                    }
-                                    "Update_rows:" => {
-                                        *update_rows.entry(table_map.clone()).or_insert(0) += 1;
-                                    }
-                                    "Table_map:" => {
-                                        table_map = words[10].replace("`", "");
-                                        table_id = words[14].to_string();
-                                    }
-                                    "Rotate" if words.get(10) == Some(&"to") => {
-                                        // eprintln!("222 {}", words[11])
-                                    }
-                                    _ => {}
+                                } else {
+                                    eprintln!("{}", line)
                                 }
-                            } else {
-                                eprintln!("{}", line)
-                            }
-                        } else if line.starts_with("#7") {
-                            let words: Vec<&str> = line.split_whitespace().collect();
+                            } else if line.starts_with("#7") {
+                                let words: Vec<&str> = line.split_whitespace().collect();
 
-                            if words[7] == "CRC32"
-                                && words[9] == "Rotate"
-                                && words.get(10) == Some(&"to")
+                                if words[7] == "CRC32"
+                                    && words[9] == "Rotate"
+                                    && words.get(10) == Some(&"to")
+                                {
+                                    binlog_file = words[11].to_string()
+                                    // eprintln!("{}", line)
+                                } else if words[7] == "Rotate" && words.get(8) == Some(&"to") {
+                                    // eprintln!("222 {}", words[9])
+                                    binlog_file = words[9].to_string()
+                                } else {
+                                }
+                            } else if line == "BINLOG '" {
+                                binlog_state = BinlogState::Binlog;
+                            } else if line.starts_with("###") {
+                                binlog_state = BinlogState::SqlText;
+                                continue;
+                            } else if line.starts_with("SET @@SESSION.GTID_NEXT= '")
+                                && line.ends_with("'/*!*/;")
                             {
-                                // eprintln!("{}", line)
-                            } else if words[7] == "Rotate" && words.get(8) == Some(&"to") {
-                                // eprintln!("222 {}", words[9])
                             } else {
                             }
-                        } else if line == "BINLOG '" {
-                            binlog_state = BinlogState::Binlog;
-                        } else if line.starts_with("###") {
-                            binlog_state = BinlogState::SqlText;
-                            continue;
-                        } else if line.starts_with("SET @@SESSION.GTID_NEXT= '")
-                            && line.ends_with("'/*!*/;")
-                        {
-                        } else {
+                            break;
                         }
-                    }
-                    BinlogState::Binlog => {
-                        if line == "'/*!*/;" {
-                            binlog_state = BinlogState::Normal;
-                        } else {
+                        BinlogState::Binlog => {
+                            if line == "'/*!*/;" {
+                                binlog_state = BinlogState::Normal;
+                            } else {
+                            }
+                            break;
                         }
-                    }
-                    BinlogState::SqlText => {
-                        if line.starts_with("###") {
-                        } else {
-                            binlog_state = BinlogState::Normal;
+                        BinlogState::SqlText => {
+                            if line.starts_with("###") {
+                            } else {
+                                binlog_state = BinlogState::Normal;
+                                continue;
+                            }
+                            break;
                         }
                     }
                 }
@@ -187,6 +170,14 @@ fn process_line(stdin_lock: std::io::StdinLock) {
     eprintln!("write_rows: {:?}", write_rows);
     eprintln!("update_rows: {:?}", update_rows);
     eprintln!("delete_rows: {:?}", delete_rows);
+
+    let total: i32 = write_rows.values().sum();
+
+    let json_string = format!(
+        r#"{{"Write_rows": {}, "Binlog_pos": {}, "Binlog_file": {}, "Commit":{}}}"#,
+        total, binlog_pos, binlog_file, commit
+    );
+    eprintln!("{}", json_string);
 }
 
 fn main() {
