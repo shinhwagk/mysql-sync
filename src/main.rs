c1064e7cfe0185a08832e765545f74573818cefa
gk <shinhwagk@outlook.com>
2024-04-28 22:02:11 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 enum BinlogState {
-    Normal,
+    None,
     Binlog,
     SqlText,
-    // Pos,
+    Gtid, // Pos,
 }
 struct Event_base {}
 
@@ -40,7 +40,7 @@ fn process_line(stdin_lock: std::io::StdinLock) {
     let mut delete_rows: HashMap<String, i32> = HashMap::new();
     let mut write_rows: HashMap<String, i32> = HashMap::new();
 
-    let mut binlog_state = BinlogState::Normal;
+    let mut binlog_state = BinlogState::None;
 
     let mut table_map: String = String::new();
     let mut table_id: String = String::new();
@@ -48,7 +48,7 @@ fn process_line(stdin_lock: std::io::StdinLock) {
     let mut binlog_pos: String = String::new();
     let mut binlog_file: String = String::new();
 
-    let mut commit: u32 = 0;
+    let mut stats_commit: u32 = 0;
 
     let mut last_time = Instant::now();
 
@@ -59,20 +59,20 @@ fn process_line(stdin_lock: std::io::StdinLock) {
                     println!("{}", line);
                 }
 
-                // if last_time.elapsed() >= Duration::from_secs(1) {
-                let total: i32 = write_rows.values().sum();
+                if last_time.elapsed() >= Duration::from_secs(1) {
+                    let total: i32 = write_rows.values().sum();
 
-                let json_string = format!(
-                    r#"{{"Write_rows": {}, "Binlog_pos": {}, "Binlog_file": {}, "Commit":{}}}"#,
-                    total, binlog_pos, binlog_file, commit
-                );
+                    let json_string = format!(
+                        r#"{{"Write_rows": {}, "Binlog_pos": {}, "Binlog_file": {}, "Commit":{}}}"#,
+                        total, binlog_pos, binlog_file, stats_commit
+                    );
 
-                eprintln!("{}", json_string);
-                //     last_time = Instant::now();
-                // }
+                    eprintln!("{}", json_string);
+                    last_time = Instant::now();
+                }
                 loop {
                     match binlog_state {
-                        BinlogState::Normal => {
+                        BinlogState::None => {
                             if line.starts_with("# at ") {
                                 let pos = &line[5..];
                                 binlog_pos = pos.to_string();
@@ -81,13 +81,20 @@ fn process_line(stdin_lock: std::io::StdinLock) {
                                 let words: Vec<&str> = line.split_whitespace().collect();
 
                                 if words.len() >= 10 && words[7] == "CRC32" {
-                                    match words[9] {
-                                        "Start:" | "Previous-GTIDs" | "Stop" | "GTID" => {}
-                                        "Xid" => {}
+                                    let binlog_event = words[9];
+                                    match binlog_event {
+                                        "Start:" | "Previous-GTIDs" | "Stop" => {}
+                                        "GTID" => {
+                                            binlog_state = BinlogState::Gtid;
+                                        }
+                                        "Xid" => {
+                                            stats_commit += 1;
+                                            table_id = "".to_string();
+                                        }
                                         "Query" => {
                                             if words.len() == 13 {
                                                 // eprintln!("111 {:?}", words)
-                                                commit += 1
+                                                // commit += 1
                                             } else if words.len() == 16 {
                                                 // eprintln!("xid {}", words[13])
                                             }
@@ -117,16 +124,20 @@ fn process_line(stdin_lock: std::io::StdinLock) {
                                 } else {
                                     eprintln!("{}", line)
                                 }
-                            } else if line.starts_with("#7") {
+                            } else if line.starts_with("#700101") {
                                 let words: Vec<&str> = line.split_whitespace().collect();
 
                                 if words[7] == "CRC32"
                                     && words[9] == "Rotate"
                                     && words.get(10) == Some(&"to")
                                 {
+                                    // binlog entry: #700101  0:00:00 server id 1  end_log_pos 0 CRC32 0xf1a55fa5 	Rotate to mysql-bin.000002  pos: 4
+
                                     binlog_file = words[11].to_string()
                                     // eprintln!("{}", line)
                                 } else if words[7] == "Rotate" && words.get(8) == Some(&"to") {
+                                    // binlog entry: #700101  0:00:00 server id 1  end_log_pos 0 	Rotate to mysql-bin.000001  pos: 4
+
                                     // eprintln!("222 {}", words[9])
                                     binlog_file = words[9].to_string()
                                 } else {
@@ -136,16 +147,13 @@ fn process_line(stdin_lock: std::io::StdinLock) {
                             } else if line.starts_with("###") {
                                 binlog_state = BinlogState::SqlText;
                                 continue;
-                            } else if line.starts_with("SET @@SESSION.GTID_NEXT= '")
-                                && line.ends_with("'/*!*/;")
-                            {
                             } else {
                             }
                             break;
                         }
                         BinlogState::Binlog => {
                             if line == "'/*!*/;" {
-                                binlog_state = BinlogState::Normal;
+                                binlog_state = BinlogState::None;
                             } else {
                             }
                             break;
@@ -153,8 +161,17 @@ fn process_line(stdin_lock: std::io::StdinLock) {
                         BinlogState::SqlText => {
                             if line.starts_with("###") {
                             } else {
-                                binlog_state = BinlogState::Normal;
+                                binlog_state = BinlogState::None;
                                 continue;
+                            }
+                            break;
+                        }
+                        BinlogState::Gtid => {
+                            if line.starts_with("SET @@SESSION.GTID_NEXT= '")
+                                && line.ends_with("'/*!*/;")
+                            {
+                                eprintln!("{}", line);
+                                binlog_state = BinlogState::None;
                             }
                             break;
                         }
@@ -175,7 +192,7 @@ fn process_line(stdin_lock: std::io::StdinLock) {
 
     let json_string = format!(
         r#"{{"Write_rows": {}, "Binlog_pos": {}, "Binlog_file": {}, "Commit":{}}}"#,
-        total, binlog_pos, binlog_file, commit
+        total, binlog_pos, binlog_file, stats_commit
     );
     eprintln!("{}", json_string);
 }
