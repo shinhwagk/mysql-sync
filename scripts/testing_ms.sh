71f5ba69ea5526e23198e3d7b8d3d07e4ab0d1e6
gk <shinhwagk@outlook.com>
2024-05-04 10:54:47 +0800

    fix

--- scripts/testing_ms.sh
+++ scripts/testing_ms.sh
@@ -12,26 +12,37 @@ declare ARGS_TARGET_PORT="${ARGS_SOURCE_PORT:-3306}"
 declare ARGS_TARGET_USER="${ARGS_SOURCE_USER:-root}"
 declare ARGS_TARGET_PASSWORD="${ARGS_TARGET_PASSWORD:-example}"
 
+mysql --host=${ARGS_TARGET_HOST} --port=${ARGS_TARGET_PORT} --user=${ARGS_TARGET_USER} --password=${ARGS_TARGET_PASSWORD} -e 'stop slave;'
+
+
 mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e 'SELECT version();'
 mysql --host=${ARGS_TARGET_HOST} --port=${ARGS_TARGET_PORT} --user=${ARGS_TARGET_USER} --password=${ARGS_TARGET_PASSWORD} -e 'SELECT version();'
 
 mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e 'reset master;'
 mysql --host=${ARGS_TARGET_HOST} --port=${ARGS_TARGET_PORT} --user=${ARGS_TARGET_USER} --password=${ARGS_TARGET_PASSWORD} -e 'reset master;'
 
-mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='${ARGS_SOURCE_HOST}',SOURCE_PORT=${ARGS_SOURCE_PORT},SOURCE_USER='${ARGS_SOURCE_USER}',SOURCE_PASSWORD='${ARGS_SOURCE_PASSWORD}',SOURCE_AUTO_POSITION=1;"
-mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e "START SLAVE;"
+mysql --host=${ARGS_TARGET_HOST} --port=${ARGS_TARGET_PORT} --user=${ARGS_TARGET_USER} --password=${ARGS_TARGET_PASSWORD} -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST=\"${ARGS_SOURCE_HOST}\",SOURCE_PORT=${ARGS_SOURCE_PORT},SOURCE_USER=\"${ARGS_SOURCE_USER}\",SOURCE_PASSWORD=\"${ARGS_SOURCE_PASSWORD}\",SOURCE_AUTO_POSITION=1;"
+# mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e "START SLAVE;"
 
 mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e "SHOW SLAVE STATUS;"
 
 
-echo "load data to source database"
-mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e 'CREATE DATABASE IF NOT EXISTS testdb;'
-# for testname in oltp_insert oltp_delete oltp_update_index oltp_update_non_index oltp_write_only bulk_insert; do
-for testname in oltp_insert; do
-    for action in cleanup prepare run cleanup; do
-        echo "sysbench ${testname}-${action}"
-        sysbench /usr/share/sysbench/${testname}.lua --table-size=100000 --tables=10 --threads=100 --time=10 --mysql-db=testdb --mysql-host=${ARGS_SOURCE_HOST} --mysql-port=${ARGS_SOURCE_PORT} --mysql-user=${ARGS_SOURCE_USER} --mysql-password=${ARGS_SOURCE_PASSWORD} --db-driver=mysql $action
+echo "start load data to source database"
+function sysbench_testing() {
+    local testdb=$1
+    mysql --host=${ARGS_SOURCE_HOST} --port=${ARGS_SOURCE_PORT} --user=${ARGS_SOURCE_USER} --password=${ARGS_SOURCE_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${testdb};"
+    # for testname in oltp_insert oltp_delete oltp_update_index oltp_update_non_index oltp_write_only bulk_insert; do
+    for testname in oltp_insert oltp_delete oltp_update_index; do
+        for action in cleanup prepare run cleanup; do
+            echo "sysbench ${testdb}-${testname}-${action} start."
+            sysbench /usr/share/sysbench/${testname}.lua --table-size=10000 --tables=10 --threads=100 --time=10 --mysql-db=${testdb} --mysql-host=${ARGS_SOURCE_HOST} --mysql-port=${ARGS_SOURCE_PORT} --mysql-user=${ARGS_SOURCE_USER} --mysql-password=${ARGS_SOURCE_PASSWORD} --db-driver=mysql $action >/dev/null
+            echo "sysbench ${testdb}-${testname}-${action} done."
+        done
     done
+}
+
+for dbid in `seq 1 3`; do
+    sysbench_testing "testdb_${dbid}" &
 done
 
 # ./gh-ost \
