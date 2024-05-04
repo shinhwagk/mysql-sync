2ca43f26da51bb04020de801ab4770cb4f2677b1
gk <shinhwagk@outlook.com>
2024-05-04 21:51:05 +0800

    testing

--- scripts/initContainer.sh
+++ scripts/initContainer.sh
@@ -3,11 +3,11 @@
 set -ex
 
 # install packages
-dnf install -y git jq diffutils gcc procps-ng iproute iputils telnet dnf-utils epel-release
+dnf install -y git jq diffutils gcc procps-ng iproute iputils telnet dnf-utils epel-release sysstat
 
 # install rustup
 # [[ -z $(command -v cargo) ]] && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # install mysql clients
 dnf install -y https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm
@@ -18,9 +18,9 @@ dnf install -y epel-release
 dnf -y install sysbench
 
 # install python & packages
-dnf install -y python3.11 python3.11-pip
-python3.11 -m pip install -r requirements.txt
+# dnf install -y python3.11 python3.11-pip
+# python3.11 -m pip install -r requirements.txt
 
-# install gh-ost
-curl -Lso /tmp/gh-ost.tar.gz https://github.com/github/gh-ost/releases/download/v1.1.6/gh-ost-binary-linux-amd64-20231207144046.tar.gz
-tar zxvf /tmp/gh-ost.tar.gz -C /usr/local/bin/
+# # install gh-ost
+# curl -Lso /tmp/gh-ost.tar.gz https://github.com/github/gh-ost/releases/download/v1.1.6/gh-ost-binary-linux-amd64-20231207144046.tar.gz
+# tar zxvf /tmp/gh-ost.tar.gz -C /usr/local/bin/
