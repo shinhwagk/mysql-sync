dab917768afa61157a904641a24fb722c7f956e0
gk <shinhwagk@outlook.com>
2024-06-16 02:55:54 +0000

    check

--- scripts/initContainer.centos.go.sh
+++ scripts/initContainer.centos.go.sh
@@ -5,8 +5,8 @@ curl -OL https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-# # install python stacks
-# dnf install -y python3.12 python3.12-pip
+# install python stacks
+dnf install -y python3.12 python3.12-pip
 # python3.12 -m pip install -r requirements.txt
 
 # install mysql client
