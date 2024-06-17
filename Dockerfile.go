2a1fd30a07ea7c46a138e409fb50fe3e9806798f
xu zhang <shinhwagk@outlook.com>
2024-06-17 16:28:43 +0800

    check

--- Dockerfile.go
+++ Dockerfile.go
@@ -6,7 +6,7 @@ COPY go.sum .
 COPY src/ .
 
 RUN go mod download
-RUN go build -ldflags="-s -w" -o mysql-sync ./*.go
+RUN go build -ldflags="-s -w" -o mysqlsync ./*.go
 
 FROM golang:1.22.3
 WORKDIR /app
