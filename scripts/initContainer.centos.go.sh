22188a973db027d207d9b87c0bdd0027fc94e1b9
gk <shinhwagk@outlook.com>
2024-06-20 00:14:09 +0000

    check

--- scripts/initContainer.centos.go.sh
+++ scripts/initContainer.centos.go.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 dnf install -y diffutils iproute procps unzip iputils
 
-curl -OL https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+# curl -OL https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
+# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+# export PATH=$PATH:/usr/local/go/bin
 
 # install python stacks
 dnf install -y python3.12 python3.12-pip
