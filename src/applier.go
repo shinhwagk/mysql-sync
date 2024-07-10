e7347b72d506c13144d9d467385a25cb7122dbcf
gk <shinhwagk@outlook.com>
2024-07-10 06:23:20 +0000

    debug-0.2.46

--- src/applier.go
+++ src/applier.go
@@ -404,7 +404,7 @@ func BuildDMLInsertQuery(datbaseName string, tableName string, columns []MysqlOp
 		// important
 		// []uint8{} -> gob -> []uint8(nil)
 		// 252 == tinyblob tinytext blob text mediumblob mediumtext longblob longtext
-		fmt.Printf("DEBUG: %s %d %#v %v\n", col.ColumnName, col.ColumnType, col.ColumnValue, col.ColumnValueIsNil)
+		fmt.Printf("DEBUG: %s %d %#v %v %v %v %v\n", col.ColumnName, col.ColumnType, col.ColumnValue, col.ColumnValueIsNil, col.ColumnValue == nil, col.ColumnType == 252, !col.ColumnValueIsNil)
 		if col.ColumnType == 252 && !col.ColumnValueIsNil && col.ColumnValue == nil {
 			params = append(params, []uint8{})
 		} else {
