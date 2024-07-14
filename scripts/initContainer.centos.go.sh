f9f9b9c6196c355371a4f288bcf92d9631e18d10
shinhwagk <shinhwagk@outlook.com>
2024-07-14 11:11:45 +0800

    check

--- scripts/initContainer.centos.go.sh
+++ scripts/initContainer.centos.go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dnf install -y diffutils iproute procps unzip iputils
+dnf install -y diffutils iproute procps unzip iputils yum-utils
 
 # install mysql client
 dnf install -y https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm
@@ -8,3 +8,9 @@ dnf install -y mysql-community-client-8.0.36
 # install sysbench
 dnf install -y epel-release
 dnf -y install sysbench
+
+# install python
+
+# install terraform
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform-1.9.2
