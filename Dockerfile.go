88416dec0c6ba70ba9f4bfdea3fd30d6feac6b62
xu zhang <shinhwagk@outlook.com>
2024-08-23 13:42:58 +0800

    consul-0.2.83

--- Dockerfile.go
+++ Dockerfile.go
@@ -8,10 +8,21 @@ COPY vendor vendor
 
 RUN go mod tidy
 RUN go mod vendor
-RUN go build -ldflags="-s -w" -o mysqlsync ./*.go
+# RUN go build -ldflags="-s -w" -o mysqlsync ./*.go
+RUN CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o mysqlsync ./*.go
+RUN /build/mysqlsync --help
 
-FROM alpine:3.20
+FROM scratch
 WORKDIR /app
-COPY --from=builder /build/mysqlsync /app/mysqlsync
+COPY --from=builder /build/mysqlsync .
 ENTRYPOINT ["/app/mysqlsync"]
 CMD ["--help"]
+
+
+
+# FROM alpine:3.20
+# WORKDIR /app
+# COPY --from=builder /build/mysqlsync /app/mysqlsync
+# # RUN apk add --no-cache ca-certificates
+# ENTRYPOINT ["/app/mysqlsync"]
+# CMD ["--help"]
