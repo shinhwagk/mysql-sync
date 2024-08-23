f7d5db85852c687aa3897562853fd2e2d75cf720
xu zhang <shinhwagk@outlook.com>
2024-08-23 13:57:03 +0800

    Update Dockerfile.go

--- Dockerfile.go
+++ Dockerfile.go
@@ -9,16 +9,13 @@ COPY vendor vendor
 RUN go mod tidy
 RUN go mod vendor
 # RUN go build -ldflags="-s -w" -o mysqlsync ./*.go
-RUN CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o mysqlsync ./*.go
-RUN /build/mysqlsync --help
+RUN CGO_ENABLED=0 GOOS=linux go build -a -ldflags="-s -w" -o mysqlsync ./*.go
 
 FROM scratch
 WORKDIR /app
 COPY --from=builder /build/mysqlsync .
 ENTRYPOINT ["/app/mysqlsync"]
 CMD ["--help"]
-
-
 
 # FROM alpine:3.20
 # WORKDIR /app
