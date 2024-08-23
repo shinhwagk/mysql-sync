40215abe49e4fe9788a017672c8141efaa795ccd
xu zhang <shinhwagk@outlook.com>
2024-08-23 13:10:26 +0800

    consul-0.2.81

--- Dockerfile.go
+++ Dockerfile.go
@@ -10,7 +10,7 @@ RUN go mod tidy
 RUN go mod vendor
 RUN go build -ldflags="-s -w" -o mysqlsync ./*.go
 
-FROM golang:1.22.4
+FROM alpine:3.20
 WORKDIR /app
 COPY --from=builder /build/mysqlsync .
 ENTRYPOINT ["/app/mysqlsync"]
