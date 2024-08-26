770821c7f924dcce46bb7e88be5d956755c1845c
gk <shinhwagk@outlook.com>
2024-08-26 07:27:12 +0000

    check

--- build.prod.sh
+++ build.prod.sh
@@ -3,6 +3,7 @@
 VERSION=`cat version`
 
 go mod tidy
+go mod vendor
 echo "building linux/amd64 binaries"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags="-s -w" -o "build/mysqlsync-amd64-${VERSION}" ./src/*.go
 echo "building linux/arm64 binaries"
