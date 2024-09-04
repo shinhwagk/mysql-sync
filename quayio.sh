df95f0774dbba18edff54f20a7549ccbf3ea1f23
xu zhang <shinhwagk@outlook.com>
2024-09-04 13:23:38 +0800

    Create quayio.sh

---
+++ quayio.sh
@@ -0,0 +1,4 @@
+VERSION=`cat version`
+docker pull --platform linux/amd64 docker.io/shinhwagk/mysql-sync:$VERSION
+docker tag docker.io/shinhwagk/mysql-sync:$VERSION quay.io/shinhwagk/mysql-sync:$VERSION
+docker push quay.io/shinhwagk/mysql-sync:$VERSION
