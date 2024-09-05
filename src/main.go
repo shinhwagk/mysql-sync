613b98efb767e626e1f2e2aa260b360c2c4689b0
xu zhang <shinhwagk@outlook.com>
2024-09-05 16:04:10 +0800

    consul-0.2.123

--- src/main.go
+++ src/main.go
@@ -3,30 +3,53 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 )
 
 func main() {
 	logger := NewLogger(1, "main")
 
-	config, err := LoadConfig("/etc/mysqlsync/config.yml")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	configPath := flag.String("config", "", "Path to config file.")
+	repl := flag.Bool("repl", false, "Start replication.")
+	dest := flag.Bool("dest", false, "Start destination.")
+	destName := flag.String("name", "", "Name for the destination (requires --dest).")
+
+	flag.Usage = func() {
+		fmt.Println("===============================================================")
+
+		fmt.Println("Usage of mysqlsync:")
+		fmt.Println()
+		fmt.Println("  0. Configuration file :")
+		fmt.Println("       https://github.com/shinhwagk/mysql-sync/blob/main/src/config-testing.yml")
+		fmt.Println()
+		fmt.Println("---------------------------------------------------------------")
+
+		fmt.Println("  1. Replication mode command:")
+		fmt.Println("       mysqlsync -config config.yml -repl")
+		fmt.Println()
+		fmt.Println("  2. Destination mode command:")
+		fmt.Println("       mysqlsync -config config.yml -dest -name dest1")
+		fmt.Println("===============================================================")
+		os.Exit(1)
+	}
+
+	flag.Parse()
+
+	if (*repl && *dest) || (!*repl && !*dest) || (*dest && *destName == "") {
+		flag.Usage()
+	}
+
+	config, err := LoadConfig(*configPath)
 	if err != nil {
 		logger.Error("Failed to load config: %v", err)
 		os.Exit(1)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	repl := flag.Bool("repl", false, "Start replication.")
-	dest := flag.Bool("dest", false, "Start destination.")
-	destName := flag.String("name", "", "Name for the destination.")
-
-	flag.Parse()
-
-	if *repl && *dest {
-		logger.Error("--repl and --dest cannot be used together.")
-	} else if *repl {
+	if *repl {
 		replication := NewReplication(*config)
 		replication.Start(ctx, cancel)
 		return
@@ -38,8 +61,6 @@ func main() {
 			destination.Start(ctx, cancel)
 			return
 		}
-	} else {
-		flag.Usage()
 	}
 	os.Exit(1)
 }
