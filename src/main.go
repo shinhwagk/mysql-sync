add5b3e436c7628918af2df417425d1b6276736e
xu zhang <shinhwagk@outlook.com>
2024-09-05 16:09:42 +0800

    consul-0.2.124

--- src/main.go
+++ src/main.go
@@ -19,27 +19,30 @@ func main() {
 	destName := flag.String("name", "", "Name for the destination (requires --dest).")
 
 	flag.Usage = func() {
-		fmt.Println("===============================================================")
+		fmt.Println("================================================================")
 
 		fmt.Println("Usage of mysqlsync:")
 		fmt.Println()
 		fmt.Println("  0. Configuration file :")
 		fmt.Println("       https://github.com/shinhwagk/mysql-sync/blob/main/src/config-testing.yml")
 		fmt.Println()
-		fmt.Println("---------------------------------------------------------------")
+		fmt.Println("----------------------------------------------------------------")
 
 		fmt.Println("  1. Replication mode command:")
 		fmt.Println("       mysqlsync -config config.yml -repl")
 		fmt.Println()
 		fmt.Println("  2. Destination mode command:")
 		fmt.Println("       mysqlsync -config config.yml -dest -name dest1")
-		fmt.Println("===============================================================")
+		fmt.Println("================================================================")
 		os.Exit(1)
 	}
 
 	flag.Parse()
 
 	if (*repl && *dest) || (!*repl && !*dest) || (*dest && *destName == "") {
+		fmt.Println()
+		fmt.Println("================================================================")
+		fmt.Println("Error: Invalid parameter configuration. Please check your input.")
 		flag.Usage()
 	}
 
